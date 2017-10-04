9cf1c0c55f350e623240fdb9d3d61d8153688357
hiradimir <yutaka@hiradimir.com>
2017-10-04 23:38:20 +0900

    prepare mysql

--- src/ts/main/index.ts
+++ src/ts/main/index.ts
@@ -30,23 +30,13 @@ interface IDescribedAttribute {
   primaryKey: boolean
 }
 
-/**
- * Validate schema of models.
- *
- * @param {Object} [options={}]
- * @param {String[]|function} [options.exclude=[`sequelizeMeta`]] if you want to skip validate table.
- * @param {Boolean|function} [options.logging=console.log] A function that logs sql queries, or false for no logging
- * @return {Promise}
- */
-export const validateSchemas = (sequelize: any, options?) => {
 
-  options = _.clone(options) || {};
-  options = _.defaults(options, {exclude: ['SequelizeMeta']}, sequelize.options);
-
-  const queryInterface = sequelize.getQueryInterface();
-
+const dataTypeToDBTypeDialect: {
+  [index: string]: (attr: IModelAttribute) => string
+} = {
   // FIXME: necessary to support any dialect
-  const dataTypeToDBType = (attr: IModelAttribute) => {
+
+  postgres: (attr: IModelAttribute) => {
 
     // this support only postgres
     if (attr.type instanceof Sequelize.STRING) {
@@ -62,7 +52,44 @@ export const validateSchemas = (sequelize: any, options?) => {
     } else {
       console.error(`${attr.field} is not support schema type.\n${JSON.stringify(attr)}`);
     }
-  };
+  },
+  mysql: (attr: IModelAttribute) => {
+
+    // this support only postgres
+    if (attr.type instanceof Sequelize.STRING) {
+      return `VARCHAR(${attr.type._length})`;
+    } else if (attr.type instanceof Sequelize.INTEGER) {
+      return 'INT(11)';
+    } else if (attr.type instanceof Sequelize.DATE) {
+      return 'DATETIME';
+    } else if (attr.type instanceof <any>Sequelize.DATEONLY) {
+      return 'DATE';
+    } else if (attr.type instanceof Sequelize.BIGINT) {
+      return 'BIGINT(20)';
+    } else {
+      console.error(`${attr.field} is not support schema type.\n${JSON.stringify(attr)}`);
+    }
+  }
+};
+
+
+/**
+ * Validate schema of models.
+ *
+ * @param {Object} [options={}]
+ * @param {String[]|function} [options.exclude=[`sequelizeMeta`]] if you want to skip validate table.
+ * @param {Boolean|function} [options.logging=console.log] A function that logs sql queries, or false for no logging
+ * @return {Promise}
+ */
+export const validateSchemas = (sequelize: any, options?) => {
+
+  options = _.clone(options) || {};
+  options = _.defaults(options, {exclude: ['SequelizeMeta']}, sequelize.options);
+
+  const queryInterface = sequelize.getQueryInterface();
+
+
+  const dataTypeToDBType = dataTypeToDBTypeDialect[sequelize.options.dialect];
 
   const checkAttributes = (queryInterface, tableName, model, options) => {
     return queryInterface.describeTable(tableName, options)
@@ -85,6 +112,10 @@ export const validateSchemas = (sequelize: any, options?) => {
     return sequelize.query(queryInterface.QueryGenerator.getForeignKeysQuery(tableName), options)
       .then((foreignKeys: Array<any>) => {
         return Promise.each(foreignKeys, (fk: any) => {
+          if(sequelize.options.dialect === 'mysql'){
+            //TODO: sequelize mysql foreign key query is not supported
+            return;
+          }
           const modelAttr: IModelAttribute = model.attributes[fk.from.split('\"').join('')];
           assert(!_.isUndefined(modelAttr.references), `${tableName}.[${modelAttr.field}] must be defined foreign key.\n${JSON.stringify(fk, null, 2)}`);
           assert(fk.to === modelAttr.references.key, `${tableName}.${modelAttr.field} => ${modelAttr.references.key} must be same to foreignKey [${fk.to}].\n${JSON.stringify(fk, null, 2)}`);
